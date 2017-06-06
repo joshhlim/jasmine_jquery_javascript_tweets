c7293f896d3edadab5eb5aae1a616fc2c308c2df
Josh Lim & Quyen Tran <pair+joshhlim.quyenttran@devbootcamp.com>
2017-06-06 14:43:38 -0700

    Replace twitter with tweety

--- app/assets/javascripts/tweet.js
+++ app/assets/javascripts/tweet.js
@@ -1,4 +1,16 @@
 function Tweet(content, username) {
   this.content = content;
   this.username = username;
+  this.isValid = function() {
+    if (this.content.length > 140) {
+      return false;
+    } else if (/(twitter)/.test(this.content)) {
+      var newContent = this.content.replace(/(twitter)/, "tweety");
+      this.content = newContent
+    }
+    return true;
+  }
 }
+
+
+
--- spec/javascripts/tweet_spec.js
+++ spec/javascripts/tweet_spec.js
@@ -1,10 +1,24 @@
 describe('a tweet', function() {
   beforeEach(function() {
-    var tweet = new Tweet('first tweet!', 'quyen');
-  })
+    tweet = new Tweet('first tweet!', 'quyen');
+    longTweet = new Tweet('i dont know its like two paragraphs of alot of typing your chocolate croissant looks a tad dry how is it it tastes peanut buttery with some chocolate lolololololollololol crusts yum :O', 'quyen');
+    trademarkTweet = new Tweet('fake twitter', 'quyen');
+  });
 
   it("has the property content", function() {
-
     expect(tweet.content).toEqual('first tweet!');
+  });
+
+  it("has a username", function() {
+    expect(tweet.username).toEqual('quyen');
+  });
+
+  it("has a character limit for content", function() {
+    expect(longTweet.isValid()).toEqual(false);
+  });
+
+  it("replaces twitter with tweety", function() {
+    trademarkTweet.isValid();
+    expect(trademarkTweet.content).toEqual("fake tweety");
   });
 })
