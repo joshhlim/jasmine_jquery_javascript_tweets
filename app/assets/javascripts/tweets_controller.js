0c872245803200fddb5701ae0239bc56307d1ca8
Jared Graham <email": null>
2017-06-06 18:06:36 -0700

    Errors are red

--- app/assets/javascripts/tweets_controller.js
+++ app/assets/javascripts/tweets_controller.js
@@ -1,22 +1,23 @@
 $(document).ready(function() {
   $("#tweet-content").on('keyup', function() {
+    var $that = $(this)
     var contentLength = $(this).val().length
-    $(".char-count").text("Character Count: " + contentLength)
-    if ($(this).val().length > 140) {
+    $("#chars").text("Character Count: " + contentLength)
+
+    if ($that.val().length > 140) {
       $("#length-err").text("Tweets have a max of 140 characters.")
     } else {
       $("#length-err").empty()
     }
-    if (($(this).val().search(/twitter/i)) > -1) {
-      var content = $(this).val().replace(/twitter/i, "tweety")
-      $(this).val(content)
+
+    if ($that.val().search(/twitter/i) > -1) {
+      var content = $that.val().replace(/twitter/i, "tweety")
+      $that.val(content)
       $("#twitter-err").text("'Twitter' not allowed in tweets; replaced with 'tweety'")
     }
 
-    if (($(this).val().search(/t.co/i)) > -1) {
+    if ($that.val().search(/t.co/i) > -1) {
       $("#tco-err").text("Links to t.co are not allowed")
-    } else {
-      $("#tco-err").empty()
     }
   })
 
@@ -24,7 +25,7 @@ $(document).ready(function() {
     var tweet = new Tweet($("#username").val(), $("#tweet-content").val())
     if (!(tweet.isValid())) {
       e.preventDefault();
-      alert("Invalid tweet")
+      alert("Tweet failed! Please read the errors for more information.")
     }
   })
 })
